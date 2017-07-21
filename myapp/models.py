6d0a33c4a04ac181db7d66fb86e9b149dcae7fa0
Akashdeep Singh Gill <akash1177@icloud.com>
2017-07-21 13:08:58 +0530

    creating an app named myapp

--- myapp/models.py
+++ myapp/models.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    Phone = models.CharField(max_length=30)
+    has_verified_phone = models.BooleanField(default=False)
+    age = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
